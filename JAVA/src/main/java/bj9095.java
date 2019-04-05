d7ae3318b34efabb1900d50f15ab3ea6a110d97d
pci2676 <pci2676@gmail.com>
2019-04-05 20:35:03 +0900

    bj2579 PASS 2/99 19.4.5
    동적계획법 진짜 감이안잡히네
    dfs로 풀어버리면 시간초과 뜨고

--- JAVA/src/main/java/bj9095.java
+++ JAVA/src/main/java/bj9095.java
@@ -1,37 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class bj9095 {
-
-    static int answer;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.valueOf(br.readLine());
-
-        for (int i = 1; i <= t; i++) {
-            answer = 0;
-            int n = Integer.valueOf(br.readLine());
-            dfs(0, n);
-
-            System.out.println(answer);
-        }
-
-    }
-
-    private static void dfs(int now, int n) {
-        if (now > n) {
-            return;
-        }
-        if (now == n) {
-            answer++;
-            return;
-        }
-
-        dfs(now+1,n);
-        dfs(now+2,n);
-        dfs(now+3,n);
-
-    }
-}
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//public class bj9095 {
+//
+//    static int answer;
+//
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int t = Integer.valueOf(br.readLine());
+//
+//        for (int i = 1; i <= t; i++) {
+//            answer = 0;
+//            int n = Integer.valueOf(br.readLine());
+//            dfs(0, n);
+//
+//            System.out.println(answer);
+//        }
+//
+//    }
+//
+//    private static void dfs(int now, int n) {
+//        if (now > n) {
+//            return;
+//        }
+//        if (now == n) {
+//            answer++;
+//            return;
+//        }
+//
+//        dfs(now+1,n);
+//        dfs(now+2,n);
+//        dfs(now+3,n);
+//
+//    }
+//}
