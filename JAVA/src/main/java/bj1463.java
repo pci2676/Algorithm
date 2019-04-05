0bc5160e15de919198742fe7d918ce6bc46dffe7
pci2676 <pci2676@gmail.com>
2019-04-05 19:31:38 +0900

    bj9095 PASS 1/99 19.4.5

--- JAVA/src/main/java/bj1463.java
+++ JAVA/src/main/java/bj1463.java
@@ -1,41 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class bj1463 {
-
-    //이전 시도 +1회
-    private static int TRY = 1;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] dp = new int[1000001];
-
-        int n = Integer.valueOf(br.readLine());
-
-        dp[1] = 0;
-        dp[2] = 1;
-        dp[3] = 1;
-
-        for (int i = 4; i <= n; i++) {
-            dp[i] = dp[i - 1] + TRY;
-
-            if (i % 3 == 0) {
-                if (dp[i] > dp[i / 3] + TRY) {
-                    dp[i] = dp[i / 3] + TRY;
-                }
-            }
-
-            if (i % 2 == 0) {
-                if (dp[i] > dp[i / 2] + TRY) {
-                    dp[i] = dp[i / 2] + TRY;
-                }
-            }
-
-        }
-
-
-        System.out.println(dp[n]);
-    }
-
-}
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//public class bj1463 {
+//
+//    //이전 시도 +1회
+//    private static int TRY = 1;
+//
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int[] dp = new int[1000001];
+//
+//        int n = Integer.valueOf(br.readLine());
+//
+//        dp[1] = 0;
+//        dp[2] = 1;
+//        dp[3] = 1;
+//
+//        for (int i = 4; i <= n; i++) {
+//            dp[i] = dp[i - 1] + TRY;
+//
+//            if (i % 3 == 0) {
+//                if (dp[i] > dp[i / 3] + TRY) {
+//                    dp[i] = dp[i / 3] + TRY;
+//                }
+//            }
+//
+//            if (i % 2 == 0) {
+//                if (dp[i] > dp[i / 2] + TRY) {
+//                    dp[i] = dp[i / 2] + TRY;
+//                }
+//            }
+//
+//        }
+//
+//
+//        System.out.println(dp[n]);
+//    }
+//
+//}
