2ce7ab78cc6113b8f2fbcbb25e9704a1c5d3ed60
pci2676 <pci2676@gmail.com>
2019-04-09 01:46:19 +0900

    bj5585 PASS 1/99 19.4.9

--- JAVA/src/main/java/baekjoon/bj11399.java
+++ JAVA/src/main/java/baekjoon/bj11399.java
@@ -1,30 +1,30 @@
-package baekjoon;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
-public class bj11399 {
-    private static final String SPLITER = " ";
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readLine());
-
-        String line = br.readLine();
-        String[] lineArr = line.split(SPLITER);
-        int[] arr = Arrays.stream(lineArr)
-                .mapToInt(Integer::valueOf)
-                .sorted()
-                .toArray();
-
-        for (int i = 1; i < n; i++) {
-            arr[i] = arr[i] + arr[i - 1];
-        }
-
-        int answer = Arrays.stream(arr)
-                .sum();
-
-        System.out.println(answer);
-    }
-}
+//package baekjoon;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//
+//public class bj11399 {
+//    private static final String SPLITER = " ";
+//
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.valueOf(br.readLine());
+//
+//        String line = br.readLine();
+//        String[] lineArr = line.split(SPLITER);
+//        int[] arr = Arrays.stream(lineArr)
+//                .mapToInt(Integer::valueOf)
+//                .sorted()
+//                .toArray();
+//
+//        for (int i = 1; i < n; i++) {
+//            arr[i] = arr[i] + arr[i - 1];
+//        }
+//
+//        int answer = Arrays.stream(arr)
+//                .sum();
+//
+//        System.out.println(answer);
+//    }
+//}
