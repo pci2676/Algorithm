1c10a7e8a4550b9ecd7c19a4d2628806e4285fd4
pci2676 <pci2676@gmail.com>
2019-04-14 18:04:11 +0900

    bj11507 PASS 1/99 19.4.14
    전형적인 dp문제라한다.
    dp문제는 풀때마다 느끼지만 점화식을 찾는다면 어렵지않게 구현할 수
    있는것같다.
    문제는 점화식을 찾는것인데
    점화식을 찾을때 어디서부터 규칙을 찾을지 고정할 포인트를 찾는게 관건인것
    같다.

--- JAVA/src/main/java/baekjoon/bj14501_DP.java
+++ JAVA/src/main/java/baekjoon/bj14501_DP.java
@@ -1,39 +1,39 @@
-package baekjoon;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
-public class bj14501_DP {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    public static void main(String[] args) throws Exception {
-        int end = Integer.valueOf(br.readLine());
-        int T[] = new int[end + 15];
-        int P[] = new int[end + 15];
-        int dp[] = new int[end + 15];
-
-        for (int day = 1; day <= end; day++) {
-            String line = br.readLine();
-            int[] dayAndWork = Arrays.stream(line.split(" "))
-                    .mapToInt(Integer::valueOf)
-                    .toArray();
-            T[day] = dayAndWork[0];
-            P[day] = dayAndWork[1];
-        }
-
-        for (int day = end; day >= 1; day--) {
-            int totalDay = day + T[day];
-
-            if (totalDay <= end + 1) {
-                dp[day] = Math.max(P[day] + dp[totalDay], dp[day+1]);
-            } else {
-                dp[day] = dp[day + 1];
-            }
-        }
-
-        int answer = dp[1];
-
-        System.out.println(answer);
-    }
-}
+//package baekjoon;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//
+//public class bj14501_DP {
+//    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//    public static void main(String[] args) throws Exception {
+//        int end = Integer.valueOf(br.readLine());
+//        int T[] = new int[end + 15];
+//        int P[] = new int[end + 15];
+//        int dp[] = new int[end + 15];
+//
+//        for (int day = 1; day <= end; day++) {
+//            String line = br.readLine();
+//            int[] dayAndWork = Arrays.stream(line.split(" "))
+//                    .mapToInt(Integer::valueOf)
+//                    .toArray();
+//            T[day] = dayAndWork[0];
+//            P[day] = dayAndWork[1];
+//        }
+//
+//        for (int day = end; day >= 1; day--) {
+//            int totalDay = day + T[day];
+//
+//            if (totalDay <= end + 1) {
+//                dp[day] = Math.max(P[day] + dp[totalDay], dp[day+1]);
+//            } else {
+//                dp[day] = dp[day + 1];
+//            }
+//        }
+//
+//        int answer = dp[1];
+//
+//        System.out.println(answer);
+//    }
+//}
