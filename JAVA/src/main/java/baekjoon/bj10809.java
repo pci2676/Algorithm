36a5c1fd5e7db6ce10408ffa8414d5d37c21035a
pci2676 <pci2676@gmail.com>
2019-04-14 23:01:01 +0900

    bj 쉬운문제 4개 풀었음 19.4.14

---
+++ JAVA/src/main/java/baekjoon/bj10809.java
@@ -0,0 +1,37 @@
+//package baekjoon;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class bj10809 {
+//
+//    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//    public static void main(String[] args) throws Exception {
+//        String line = br.readLine();
+//        String[] arr = line.split("");
+//        Map<Integer, Integer> map;
+//        map = putMap(arr);
+//
+//        for (int i = 'a'; i <= 'z'; i++) {
+//            if(map.containsKey(i)){
+//                System.out.print(map.get(i)+" ");
+//            }else{
+//                System.out.print(-1+" ");
+//            }
+//        }
+//    }
+//
+//    private static Map<Integer, Integer> putMap(String[] arr) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < arr.length; i++) {
+//            if (map.containsKey((int)arr[i].charAt(0))) {
+//                continue;
+//            }
+//            map.put((int)arr[i].charAt(0), i);
+//        }
+//        return map;
+//    }
+//}
---
+++ JAVA/src/main/java/baekjoon/bj2750.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class bj2750 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue();
+
+        int n = Integer.valueOf(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int number = Integer.valueOf(br.readLine());
+            priorityQueue.add(number);
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(priorityQueue.poll());
+        }
+    }
+}
