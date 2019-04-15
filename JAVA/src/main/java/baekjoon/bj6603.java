5e95005971c100d20b6ed2dfdf0a7fb8d0d56e4e
pci2676 <pci2676@gmail.com>
2019-04-16 02:04:37 +0900

    bj6603 PASS 1/99 19.4.16
    브루트포스 문제였고 n의 수가 적어서 dfs로 풀었다.
    예외점을 잘 잡아냈으면 금방 풀었을 문제였는데 너무 헤맸다.

---
+++ JAVA/src/main/java/baekjoon/bj6603.java
@@ -0,0 +1,69 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class bj6603 {
+
+    static List<Integer> answer;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] test = new int[1];
+        test[0] = 1;
+
+        while (test[0] != 0) {
+            String line = br.readLine();
+            String[] arr = line.split(" ");
+            test = Arrays.stream(arr)
+                    .mapToInt(Integer::valueOf)
+                    .toArray();
+
+            int length = test[0];
+            int[] numbers = new int[length];
+            for (int i = 1; i <= length; i++) {
+                numbers[i - 1] = test[i];
+            }
+
+            for (int i = 0; i < length; i++) {
+                if (length - i == 5) {
+                    break;
+                }
+                answer = new ArrayList<>();
+                dfs(numbers, i, length);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void dfs(int[] numbers, int index, int length) {
+        answer.add(numbers[index]);
+
+        if (answer.size() == 6) {
+            for (int i = 0; i < 6; i++) {
+                System.out.print(answer.get(i) + " ");
+            }
+            System.out.println();
+            deleteEnd();
+            return;
+        }
+
+        for (int i = 1; i < length; i++) {
+            if (i + index >= length) {
+                break;
+            }
+            dfs(numbers, index + i, length);
+        }
+        deleteEnd();
+    }
+
+    private static void deleteEnd() {
+        int last = answer.size() - 1;
+        answer.remove(last);
+    }
+}
