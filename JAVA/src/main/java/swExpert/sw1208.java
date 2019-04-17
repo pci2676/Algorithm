e946c3355c96eb31e6794f2cf72bea3b98519eac
pci2676 <pci2676@gmail.com>
2019-04-18 03:27:29 +0900

    sw2806 PASS 2/99 19.4.18
    사실 간단한 DFS 문제였는데 너무 멍청하게 생각했다
    진짜 연습만이 살길이다 찬인아

--- JAVA/src/main/java/swExpert/sw1208.java
+++ JAVA/src/main/java/swExpert/sw1208.java
@@ -1,59 +1,59 @@
-package swExpert;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
-public class sw1208 {
-
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    public static void main(String[] args) throws Exception {
-
-        for (int test = 0; test < 10; test++) {
-
-            int dumps = Integer.valueOf(br.readLine());
-
-            int[] heights = Arrays.stream(br.readLine()
-                    .split(" "))
-                    .mapToInt(Integer::valueOf)
-                    .toArray();
-
-
-            for (int t = 0; t < dumps; t++) {
-
-                int max = Arrays.stream(heights).max().getAsInt();
-                int min = Arrays.stream(heights).min().getAsInt();
-
-                if (max == min) {
-                    break;
-                }
-
-                boolean maxFlag = true;
-                boolean minFlag = true;
-
-                for (int i = 0; i < 100; i++) {
-                    if (!maxFlag && !minFlag) {
-                        break;
-                    }
-                    if (heights[i] == max && maxFlag) {
-                        heights[i]--;
-                        maxFlag = false;
-                    }
-                    if (heights[i] == min && minFlag) {
-                        heights[i]++;
-                        minFlag = false;
-                    }
-                }
-            }
-
-            int finalMax = Arrays.stream(heights).max().getAsInt();
-            int finalMin = Arrays.stream(heights).min().getAsInt();
-
-            System.out.println("# " + (test+1) + " " + (finalMax - finalMin));
-
-        }
-
-
-    }
-}
+//package swExpert;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//
+//public class sw1208 {
+//
+//    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//    public static void main(String[] args) throws Exception {
+//
+//        for (int test = 0; test < 10; test++) {
+//
+//            int dumps = Integer.valueOf(br.readLine());
+//
+//            int[] heights = Arrays.stream(br.readLine()
+//                    .split(" "))
+//                    .mapToInt(Integer::valueOf)
+//                    .toArray();
+//
+//
+//            for (int t = 0; t < dumps; t++) {
+//
+//                int max = Arrays.stream(heights).max().getAsInt();
+//                int min = Arrays.stream(heights).min().getAsInt();
+//
+//                if (max == min) {
+//                    break;
+//                }
+//
+//                boolean maxFlag = true;
+//                boolean minFlag = true;
+//
+//                for (int i = 0; i < 100; i++) {
+//                    if (!maxFlag && !minFlag) {
+//                        break;
+//                    }
+//                    if (heights[i] == max && maxFlag) {
+//                        heights[i]--;
+//                        maxFlag = false;
+//                    }
+//                    if (heights[i] == min && minFlag) {
+//                        heights[i]++;
+//                        minFlag = false;
+//                    }
+//                }
+//            }
+//
+//            int finalMax = Arrays.stream(heights).max().getAsInt();
+//            int finalMin = Arrays.stream(heights).min().getAsInt();
+//
+//            System.out.println("# " + (test+1) + " " + (finalMax - finalMin));
+//
+//        }
+//
+//
+//    }
+//}
