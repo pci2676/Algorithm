d5154e327f350a0650ea6158197ea9667e45578d
pci2676 <pci2676@gmail.com>
2019-04-20 21:16:54 +0900

    bj1152 PASS 4/99 19.4.20
    문자열 처리시 예외사항에 대한 생각을 많이 해볼수 있는 문제였던것 같다.

---
+++ JAVA/src/main/java/baekjoon/bj1152.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj1152 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = br.readLine();
+
+        String[] arr = line.split(" ");
+        int count = arr.length;
+
+        if(count==0){
+            System.out.println(0);
+        }
+        else{
+            if (arr[0].equals(""))
+                count--;
+            System.out.println(count);
+        }
+    }
+}
