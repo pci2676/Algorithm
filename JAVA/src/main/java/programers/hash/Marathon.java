6bec2cbe926997becdb22a092b3a5a6e706f57f2
pci2676 <pci2676@gmail.com>
2019-06-06 21:38:16 +0900

    프로그래머스 마라톤 해시맵
    정렬해서 풀면 엄청 빠르게 풀수 있는 문제였다.

---
+++ JAVA/src/main/java/programers/hash/Marathon.java
@@ -0,0 +1,40 @@
+package programers.hash;
+
+import java.util.Arrays;
+
+public class Marathon {
+    class Solution {
+        public String solution(String[] participant, String[] completion) {
+            String answer = "";
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden", "eden"};
+        String[] completion = {"kiki", "eden", "leo"};
+        String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < participant.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
+        }
+
+//        LinkedList<String> runner = new LinkedList<>(Arrays.asList(participant));
+//        List<String> done = new LinkedList<>(Arrays.asList(completion));
+//
+//        done.parallelStream()
+//                .forEach(one -> runner.removeFirstOccurrence(one));
+//
+//        answer = runner.getFirst();
+
+        System.out.println(answer);
+    }
+
+
+}
