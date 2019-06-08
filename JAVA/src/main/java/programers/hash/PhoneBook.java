c233f63199e3185612bf96a7446273de69ea3ea2
pci2676 <pci2676@gmail.com>
2019-06-08 18:03:09 +0900

    프로그래머스 전화번호부 해시쓰라고함
    근데 startWith이라는 좋은 함수가 있어서 해시를 어떻게 쓰라는건지
    모르겠음

---
+++ JAVA/src/main/java/programers/hash/PhoneBook.java
@@ -0,0 +1,42 @@
+package programers.hash;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PhoneBook {
+    class Solution {
+        public boolean solution(String[] phone_book) {
+            boolean answer = true;
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] phone_book = {"123", "456", "789"};
+        boolean answer = true;
+
+        List<String> book = new LinkedList<>(Arrays.asList(phone_book));
+
+        for (int i = 0; i < phone_book.length; i++) {
+            String prefix = phone_book[i];
+
+            for (int j = 0; j < phone_book.length; j++) {
+                String compare = phone_book[j];
+                if (prefix.equals(compare)) {
+                    continue;
+                }
+                if (compare.startsWith(prefix)) {
+                    answer = false;
+                    break;
+                }
+            }
+            if (!answer) {
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
