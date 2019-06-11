4bb4fbcbbc67114357f76e752438bce377cc0ed1
pci2676 <pci2676@gmail.com>
2019-06-11 16:55:05 +0900

    프로그래머스 가벼운 완전탐색 문제

---
+++ JAVA/src/main/java/programers/bruteForce/PresentNumber.java
@@ -0,0 +1,29 @@
+package programers.bruteForce;
+
+public class PresentNumber {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 15;
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+        }
+
+    }
+}
