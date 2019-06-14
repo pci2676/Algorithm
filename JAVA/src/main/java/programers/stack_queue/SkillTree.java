e863f19df0c3753721f6ccff911de0ee4ff4182f
pci2676 <pci2676@gmail.com>
2019-06-14 17:04:26 +0900

    프로그래머스 윈터캠프 큐 쓰고싶어서 큐 썻음

---
+++ JAVA/src/main/java/programers/stack_queue/SkillTree.java
@@ -0,0 +1,50 @@
+package programers.stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SkillTree {
+
+    public static void main(String[] args) {
+        int answer = 0;
+        String skill = "CBD";
+        String[] skill_trees = {"BACDE", "CBADF", "BDA", "AECB"};
+
+        Queue<Character> need = new LinkedList<>();
+        for (int i = 0; i < skill.length(); i++) {
+            need.offer(skill.charAt(i));
+        }
+
+        for (int i = 0; i < skill_trees.length; i++) {
+
+            boolean flag = true;
+            Queue<Character> copy = new LinkedList<>(need);
+
+            for (int j = 0; j < skill_trees[i].length() && !copy.isEmpty(); j++) {
+                Character now = skill_trees[i].charAt(j);
+                if (!copy.contains(now)) {
+                    continue;
+                }
+                Character must = copy.poll();
+                if (must != now) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                answer++;
+            }
+
+        }
+
+        System.out.println(answer);
+    }
+
+    class Solution {
+        public int solution(String skill, String[] skill_trees) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
