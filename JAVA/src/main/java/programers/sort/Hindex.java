a900ba452db30270b79dabe53d80bb8fe4ebc34a
pci2676 <pci2676@gmail.com>
2019-06-17 23:26:35 +0900

    프로그래머스 큐 정렬
    Arrays.sort를 쓰면 머지 소트가 쫙!

---
+++ JAVA/src/main/java/programers/sort/Hindex.java
@@ -0,0 +1,49 @@
+package programers.sort;
+
+import java.util.Arrays;
+
+public class Hindex {
+    public static void main(String[] args) {
+        int[] citations = {1, 2, 3, 4, 5, 5, 5, 5, 6, 7, 7, 7, 7, 7};
+
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        int hIndex = 0;
+
+        for (int i = 0; i < citations[citations.length - 1]; i++) {
+            int small = 0;
+            int big = 0;
+            int before = hIndex;
+
+            hIndex = i;
+
+            for (int j = 0; j < citations.length; j++) {
+                if (hIndex == citations[j]) {
+                    continue;
+                }
+                if (hIndex > citations[j]) {
+                    small++;
+                }
+            }
+            big = citations.length - small;
+            if (big < hIndex || small > hIndex) {
+                hIndex = before;
+                break;
+            }
+        }
+        answer = hIndex;
+
+        System.out.println(answer);
+
+    }
+
+    class Solution {
+        public int solution(int[] citations) {
+            int answer = 0;
+            return answer;
+        }
+    }
+
+}
