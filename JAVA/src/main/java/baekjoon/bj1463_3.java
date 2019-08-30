9aa6a2d29f810faf7b62ee319cd07ffd5cff0c6f
chanin <pci2676@gmail.com>
2019-08-30 22:23:19 +0900

    bj1463_3 1로 만들기
    1로 만들기를 이넘을 이용해서 풀어보았다.
    이넘을 써서 인지 메모리초과는 나지만 확장성이 더 뛰어나 진것 같다.

---
+++ JAVA/src/main/java/baekjoon/bj1463_3.java
@@ -0,0 +1,64 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class bj1463_3 {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        Integer[] dp = new Integer[n + 1];
+
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i <= n; i++) {
+            Integer minByOne;
+
+            minByOne = dp[i - 1];
+
+            Integer divideMin = Divide.getMin(dp, i);
+
+            dp[i] = getMin(minByOne, divideMin) + 1;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+
+    private static Integer getMin(Integer min1, Integer min2) {
+        if (min1 > min2) {
+            return min2;
+        }
+        return min1;
+    }
+
+    enum Divide {
+        THREE(x -> x / 3, x -> x % 3 == 0),
+        TWO(x -> x / 2, x -> x % 2 == 0);
+
+        private Function<Integer, Integer> execute;
+        private Function<Integer, Boolean> canDivide;
+
+        Divide(Function<Integer, Integer> execute, Function<Integer, Boolean> canDivide) {
+            this.execute = execute;
+            this.canDivide = canDivide;
+        }
+
+        public static Integer getMin(Integer[] dp, int target) {
+            return Arrays.stream(values())
+                    .filter(divideEnum -> divideEnum.canDivide.apply(target))
+                    .map(divideEnum -> divideEnum.execute.apply(target))
+                    .map(before -> dp[before])
+                    .min(Integer::compareTo)
+                    .orElse(9999999);
+        }
+
+    }
+
+}
