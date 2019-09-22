d5afe4639389cf9b08258479335593672a3a3b24
chanin <pci2676@gmail.com>
2019-09-23 00:42:17 +0900

    Programers 더맵게
    힙을 이용해야하는 문제는 우선순위큐로 효율성 통과가 가능해진다.

--- JAVA/src/main/java/programers/heap/MoreSpicy.java
+++ JAVA/src/main/java/programers/heap/MoreSpicy.java
@@ -1,72 +1,47 @@
 package programers.heap;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class MoreSpicy {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] scovile = {0, 0, 3, 9, 10, 12};
-        System.out.println(solution.solution(scovile, 13));
+        int[] scovile = {1, 2, 3, 9, 10, 12};
+        System.out.println(solution.solution(scovile, 7));
     }
 
     static class Solution {
         public int solution(int[] scoville, int K) {
 
             int answer = 0;
-            List<Integer> scovilles = new LinkedList<>();
-            Arrays.sort(scoville);
+            Queue<Integer> scovilles = new PriorityQueue<>();
             for (int each : scoville) {
-                scovilles.add(each);
+                scovilles.offer(each);
             }
-
-            Mixer mixer = new Mixer();
 
             while (!isReach(scovilles, K)) {
                 if (scovilles.size() == 1) {
                     return -1;
                 }
-                System.out.println(scovilles);
+                mix(scovilles);
                 answer++;
-                mixer.mix(scovilles);
             }
-            System.out.println(scovilles);
 
             return answer;
         }
 
-        private boolean isReach(List<Integer> scoville, int K) {
-            for (Integer sco : scoville) {
-                if (sco < K) {
-                    return false;
-                }
-            }
-            return true;
+        private void mix(Queue<Integer> scovilles) {
+            Integer min = scovilles.poll();
+            Integer nextMin = scovilles.poll();
+            Integer mixed = min + nextMin * 2;
+            scovilles.offer(mixed);
+        }
+
+        private boolean isReach(Queue<Integer> scovilles, int K) {
+            return scovilles.peek() >= K;
         }
     }
 
-    static class Mixer {
 
-        public void mix(List<Integer> scovilles) {
-            Integer mostOfLower = scovilles.get(0);
-            Integer nextLower = scovilles.get(1);
-            Integer mixed = mostOfLower + nextLower * 2;
-            scovilles.remove(mostOfLower);
-            scovilles.remove(nextLower);
-            addMixed(scovilles, mixed);
-        }
-
-        private void addMixed(List<Integer> scovilles, Integer mixed) {
-            for (int i = scovilles.size() - 1; i >= 0; i--) {
-                if (scovilles.get(i) < mixed) {
-                    scovilles.add(i + 1, mixed);
-                    return;
-                }
-            }
-            scovilles.add(0, mixed);
-        }
-
-    }
 }
